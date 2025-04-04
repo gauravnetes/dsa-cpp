152a40d009a67d63a2e399aee81d4f3ac2f3e722
gauravnetes <gouravchandra935@gmail.com>
2025-04-04 23:40:47 +0530

    LL DLL operations

---
+++ linked-list/1D_DLL/1d_dll.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+class Node {
+    public: 
+    int data; 
+    Node* next; 
+    Node* back; 
+    public: 
+    Node(int data1, Node* next1, Node* back1) {
+        data = data1; 
+        next = next1;
+        back = back1;  
+    }
+
+    public: 
+    Node(int data1) {
+        data = data1; 
+        next = nullptr;
+        back = nullptr;  
+    }
+}; 
+
+Node* convertArrtoDLL(vector<int> &arr) {
+    Node* head = new Node(arr[0], nullptr, nullptr); 
+    Node* prev = head; 
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i], nullptr, prev); 
+        prev->next = temp; 
+        prev = temp; // prev->next
+    }
+    return head; 
+}
+
+Node* deleteTheHead(Node* head) {
+    if (head == NULL || head->next == NULL)
+    {
+        return NULL; 
+    }
+    Node* prev = head; 
+    head = head->next; 
+
+    head->back = nullptr; 
+    prev->next = nullptr; 
+
+    delete prev; 
+    return head; 
+}
+
+void print(Node* head) {
+    while (head != NULL)
+    {
+        cout << head->data << " "; 
+        head = head->next; 
+    }
+    cout << endl; 
+}
+
+int main() { 
+    vector<int> arr = {12, 5, 8, 7}; 
+
+    Node* head = convertArrtoDLL(arr); 
+    print(head); 
+
+    head = deleteTheHead(head); 
+    print(head);
+    return 0; 
+}
